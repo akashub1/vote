4f0f23c2f4957c53b97710f3a9c64da1b47f9036
akaskub <akashub666@gmail.com>
2018-04-17 14:15:43 +0530

    vote

--- admin.php
+++ admin.php
@@ -1,54 +1,47 @@
+<html>
+<head>
+<style>
+body{
+	background-color: "#0000FF";
+	text-align: center;
+}
+</style>
+<script>
+	var ajaxobject=new XMLHttpRequest();
+	function members() {
+	  if(!ajaxobject)
+	  {
+	  	document.getElementById("display").innerHTML="AJAX obejct could not be created";
+	  	return;
+	  }
+	  	  ajaxobject.open("GET","section.php");
+	  	  ajaxobject.send();
+	  	  ajaxobject.onreadystatechange=getResponse; 	
+	}
+	function getResponse(){
+		if(ajaxobject.readyState == 4 && ajaxobject.status == 200)
+		{
+			document.getElementById('display').innerHTML=ajaxobject.responseText;
+		}
+	}
 
+	
+</script>
+</head>
+<body>
 
-    <?php
-     
-    $dataPoints = array(
-    	array("x"=> 10, "y"=> 41),
-    	array("x"=> 20, "y"=> 35, "indexLabel"=> "Lowest"),
-    	array("x"=> 30, "y"=> 50),
-    	array("x"=> 40, "y"=> 45),
-    	array("x"=> 50, "y"=> 52),
-    	array("x"=> 60, "y"=> 68),
-    	array("x"=> 70, "y"=> 38),
-    	array("x"=> 80, "y"=> 71, "indexLabel"=> "Highest"),
-    	array("x"=> 90, "y"=> 52),
-    	array("x"=> 100, "y"=> 60),
-    	array("x"=> 110, "y"=> 36),
-    	array("x"=> 120, "y"=> 49),
-    	array("x"=> 130, "y"=> 41)
-    );
-    	
-    ?>
-    <!DOCTYPE HTML>
-    <html>
-    <head>  
-    <script>
-    window.onload = function () {
-     
-    var chart = new CanvasJS.Chart("chartContainer", {
-    	animationEnabled: true,
-    	exportEnabled: true,
-    	theme: "light1", // "light1", "light2", "dark1", "dark2"
-    	title:{
-    		text: "Simple Column Chart with Index Labels"
-    	},
-    	data: [{
-    		type: "column", //change type to bar, line, area, pie, etc
-    		//indexLabel: "{y}", //Shows y value on all Data Points
-    		indexLabelFontColor: "#5A5757",
-    		indexLabelPlacement: "outside",   
-    		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-    	}]
-    });
-    chart.render();
-     
-    }
-    </script>
-    </head>
-    <body>
-    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-    </body>
-    </html>                              
+		<h1>Welcome</h1>
+		<h4>Enter the section winner</h4>
+		
+		<form metho="POST" action="win.php">
+			<input type="button" value="Sections"onclick="members()"/>
+			
+		
 
+		<div id="display" class="displaybox">
+		</div>	
+			<input type="submit" value="Win" >
+			</form>
+</body>
+</html>
 
